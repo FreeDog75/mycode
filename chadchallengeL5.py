cdaa36611ce1a8169b8f91eda1651f56cc63112e
James Freeman <james.freeman@tlgcohort.com>
2022-01-04 18:09:23 +0000

    learning about dictionaries

---
+++ chadchallengeL5.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+char_name = input("Which character do you want to know about? (Flash, Batman, Superman)").lower
+char_stat = input("What statistic do you want to know about? (strength, speed, or intelligence)").lower
+charlist =  {"flash":
+                {"speed": "fastest", "intelligence": "lowest", "strength": "lowest"}, 
+            "batman":
+                {"speed": "slowest", "intelligence": "Highest", "strength": "Money"}, 
+            "superman":
+                {"speed": "fast", "intelligence": "average", "strength": "strongest"}}
+
+#Following command replaced by adding .lower to end of input
+#print(charlist[char_name.lower()][char_stat.lower()])
+
+
+print(f"{char_name()}'s {char_stat()} is {charlist[char_name][char_stat]}")
